e551cfe0934f62ae6f7c154bb5e640462f3b34f7
J.C <36017765+jcps90@users.noreply.github.com>
2018-10-25 17:10:29 -0700

    Add files via upload

---
+++ program2/main/java/edu/sdsu/cs/datastructures/BalancedMap.java
@@ -0,0 +1,21 @@
+package edu.sdsu.cs.datastructures;
+import java.util.TreeMap;
+
+/**
+ * Micheal Kemper
+ *
+ *
+ * Juan Pina-Sanz
+ * cssc0835
+ *
+ * @param <K>
+ * @param <V>
+ */
+
+public class BalancedMap <K extends Comparable<K>,V> implements IMap<K,V>
+{
+    public static void main( String[] args )
+    {
+
+    }
+}
---
+++ program2/main/java/edu/sdsu/cs/datastructures/UnbalancedMap.java
@@ -0,0 +1,20 @@
+package edu.sdsu.cs.datastructures;
+
+/**
+ * Micheal Kemper
+ *
+ *
+ * Juan Pina-Sanz
+ * cssc0835
+ *
+ * @param <K>
+ * @param <V>
+ */
+
+public class UnbalancedMap <K extends Comparable<K>,V> implements IMap<K,V>
+{
+    public static void main( String[] args )
+    {
+
+    }
+}
